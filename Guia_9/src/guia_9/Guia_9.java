71fe13633c7334d0c647dcdcdf3ead879ae89471
FaberVillarreal18 <faber.villarrealantequera@unicolombo.edu.co>
2024-04-21 00:02:16 -0500

    Guia 9

---
+++ Guia_9/src/guia_9/Guia_9.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package guia_9;
+
+import gui.Ventana;
+
+/**
+ *
+ * @author rodri
+ */
+public class Guia_9 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         Ventana x = new Ventana();
+         x.setVisible(true);
+    }
+    
+}
